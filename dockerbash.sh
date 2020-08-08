a60f6e1a557a36592d068a2e567f532bb6f8ba21
Bob Robey <brobey@lanl.gov>
2020-08-08 14:51:15 -0600

    More container fixes

--- dockerbash.sh
+++ dockerbash.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker build -f Dockerfile.Ubuntu20.04 -t chapter2 .
-docker run -it --entrypoint /bin/bash chapter2
+docker run -it --entrypoint /bin/bash essentialsofparallelcomputing/chapter2
